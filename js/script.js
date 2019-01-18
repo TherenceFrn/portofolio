cc568a6fa13248168a6be7d2d5a51be8f09cd51b
FERRON Therence <therence.ferron@orange.fr>
2019-01-18 16:33:31 +0100

    hover section OK

--- js/script.js
+++ js/script.js
@@ -8,27 +8,73 @@
 //     $('#imgBody').attr("src", "trigger.gif");
 //   });
 //
-
 $(".section-1").ready(function(){
-
-  $(".aside-index-1").css("visibility", "visible");
   $(".section-1").mouseenter(function() {
 
-    $(".aside-index-1").css("visibility", "visible");
+    $(".aside-index-1").css("opacity", "1");
   });
   $(".section-1").mouseleave(function() {
 
-    $(".aside-index-1").css("visibility", "hidden");
+    $(".aside-index-1").css("opacity", "0");
+  });
+});
+
+$(".section-2").ready(function(){
+  $(".section-2").mouseenter(function() {
+
+    $(".aside-index-2").css("opacity", "1");
+  });
+  $(".section-2").mouseleave(function() {
+
+    $(".aside-index-2").css("opacity", "0");
   });
 });
 
 $(".section-3").ready(function(){
   $(".section-3").mouseenter(function() {
 
-    $(".aside-index-3").css("visibility", "visible");
+    $(".aside-index-3").css("opacity", "1");
   });
   $(".section-3").mouseleave(function() {
 
-    $(".aside-index-3").css("visibility", "hidden");
+    $(".aside-index-3").css("opacity", "0");
+  });
+});
+
+
+$(".section-4").ready(function(){
+  $(".section-4").mouseenter(function() {
+
+    $(".aside-index-4").css("opacity", "1");
+  });
+  $(".section-4").mouseleave(function() {
+
+    $(".aside-index-4").css("opacity", "0");
+  });
+});
+
+
+
+$(".section-5").ready(function(){
+  $(".section-5").mouseenter(function() {
+
+    $(".aside-index-5").css("opacity", "1");
+  });
+  $(".section-5").mouseleave(function() {
+
+    $(".aside-index-5").css("opacity", "0");
+  });
+});
+
+
+
+$(".section-6").ready(function(){
+  $(".section-6").mouseenter(function() {
+
+    $(".aside-index-6").css("opacity", "1");
+  });
+  $(".section-6").mouseleave(function() {
+
+    $(".aside-index-6").css("opacity", "0");
   });
 });
