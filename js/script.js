105dda615e8222d0c83ba9506f3c44f62433d303
FERRON Therence <therence.ferron@orange.fr>
2019-01-31 12:23:30 +0100

    jjkl
    Signed-off-by: FERRON Therence <therence.ferron@orange.fr>

--- js/script.js
+++ js/script.js
@@ -106,7 +106,21 @@ $("#portofolio .section-2 .article-1").ready(function(){
 
     $("#portofolio .section-2 .article-1 .p").css("opacity", "0");
   });
+
+  $("#portofolio .section-2 .article-1").click(function() {
+
+    $( "body" ).append( '<div id="bg" class="bg"><div class="grosse-previ"><img src="https://i.imgur.com/qjONxb3.png"></div></div>');
+  });
+
+  $("#bg").click(function() {
+
+    $("body").remove("#bg");
+$( "body" ).append( '<img src="https://i.imgur.com/qjONxb3.png">');
+  });
+
 });
+
+
 
 $("#portofolio .section-2 .article-2").ready(function(){
   $("#portofolio .section-2 .article-2").mouseenter(function() {
