dd6305adc241f64db5991905a3c45e89b9a8c2e8
FERRON Therence <therence.ferron@orange.fr>
2019-01-31 14:41:31 +0100

    qsdqsd

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 //     $('#imgBody').attr("src", "trigger.gif");
 //   });
 //
-$("#index .section-1").ready(function(){
+$("#index .section-1").ready(function() {
   $("#index .section-1").mouseenter(function() {
 
     $("#index .aside-index-1").css("opacity", "1");
@@ -19,7 +19,7 @@ $("#index .section-1").ready(function(){
   });
 });
 
-$("#index .section-2").ready(function(){
+$("#index .section-2").ready(function() {
   $("#index .section-2").mouseenter(function() {
 
     $("#index .aside-index-2").css("opacity", "1");
@@ -30,7 +30,7 @@ $("#index .section-2").ready(function(){
   });
 });
 
-$("#index .section-3").ready(function(){
+$("#index .section-3").ready(function() {
   $("#index .section-3").mouseenter(function() {
 
     $("#index .aside-index-3").css("opacity", "1");
@@ -42,7 +42,7 @@ $("#index .section-3").ready(function(){
 });
 
 
-$("#index .section-4").ready(function(){
+$("#index .section-4").ready(function() {
   $("#index .section-4").mouseenter(function() {
 
     $("#index .aside-index-4").css("opacity", "1");
@@ -55,7 +55,7 @@ $("#index .section-4").ready(function(){
 
 
 
-$("#index .section-5").ready(function(){
+$("#index .section-5").ready(function() {
   $("#index .section-5").mouseenter(function() {
 
     $("#index .aside-index-5").css("opacity", "1");
@@ -68,7 +68,7 @@ $("#index .section-5").ready(function(){
 
 
 
-$("#index .section-6").ready(function(){
+$("#index .section-6").ready(function() {
   $("#index .section-6").mouseenter(function() {
 
     $("#index .aside-index-6").css("opacity", "1");
@@ -79,25 +79,25 @@ $("#index .section-6").ready(function(){
   });
 });
 
-$(".section-1").ready(function(){
-    $('.section-1').click(function(){
-      window.location = 'profil.html';
-    });
+$(".section-1").ready(function() {
+  $('.section-1').click(function() {
+    window.location = 'profil.html';
+  });
 });
 
-$(".acceuil-header").ready(function(){
-    $('.acceuil-header').click(function(){
-      window.location = 'index.html';
-    });
+$(".acceuil-header").ready(function() {
+  $('.acceuil-header').click(function() {
+    window.location = 'index.html';
+  });
 });
-$(".contact-header").ready(function(){
-    $('.contact-header').click(function(){
-      window.location = 'contact.html';
-    });
+$(".contact-header").ready(function() {
+  $('.contact-header').click(function() {
+    window.location = 'contact.html';
+  });
 });
 
 
-$("#portofolio .section-2 .article-1").ready(function(){
+$("#portofolio .section-2 .article-1").ready(function() {
 
   $("#portofolio .section-2 .article-1").mouseenter(function() {
 
@@ -110,7 +110,7 @@ $("#portofolio .section-2 .article-1").ready(function(){
 
   $("#portofolio .section-2 .article-1").click(function() {
 
-    $( "body" ).append( '<div class="bg" id="bg"><a class="close" id="close">X</a><div class="grosse-previ"><img src="https://i.imgur.com/qjONxb3.png"></div></div>');
+    $("body").append('<div class="bgport"><a class="close">X</a><div class="grosseprevi"><img src="https://i.imgur.com/qjONxb3.png"></div></div>');
   });
 
 
@@ -118,18 +118,22 @@ $("#portofolio .section-2 .article-1").ready(function(){
 
 
 
-    var box = $("#bg");
-    var btn = $("#bg");
+var box = $(".bgport");
+var btn = $(".close");
 
-      btn.on('click', function(){
+$(".bgport").ready(function() {
 
-      box.css('visibility', 'hidden');
-    });
+  btn.on('mouseover', function() {
 
+    box.css('visibility', 'hidden');
 
+    console.log('ca marche!');
 
+  });
 
-$("#portofolio .section-2 .article-2").ready(function(){
+});
+
+$("#portofolio .section-2 .article-2").ready(function() {
   $("#portofolio .section-2 .article-2").mouseenter(function() {
 
     $("#portofolio .section-2 .article-2 .p").css("opacity", "1");
@@ -140,7 +144,7 @@ $("#portofolio .section-2 .article-2").ready(function(){
   });
 });
 
-$("#portofolio .section-2 .article-3").ready(function(){
+$("#portofolio .section-2 .article-3").ready(function() {
   $("#portofolio .section-2 .article-3").mouseenter(function() {
 
     $("#portofolio .section-2 .article-3 .p").css("opacity", "1");
@@ -151,7 +155,7 @@ $("#portofolio .section-2 .article-3").ready(function(){
   });
 });
 
-$("#portofolio .section-2 .article-4").ready(function(){
+$("#portofolio .section-2 .article-4").ready(function() {
   $("#portofolio .section-2 .article-4").mouseenter(function() {
 
     $("#portofolio .section-2 .article-4 .p").css("opacity", "1");
@@ -162,7 +166,7 @@ $("#portofolio .section-2 .article-4").ready(function(){
   });
 });
 
-$("#portofolio .section-2 .article-5").ready(function(){
+$("#portofolio .section-2 .article-5").ready(function() {
   $("#portofolio .section-2 .article-5").mouseenter(function() {
 
     $("#portofolio .section-2 .article-5 .p").css("opacity", "1");
@@ -174,7 +178,7 @@ $("#portofolio .section-2 .article-5").ready(function(){
 });
 
 
-$("#portofolio .section-2 .article-6").ready(function(){
+$("#portofolio .section-2 .article-6").ready(function() {
   $("#portofolio .section-2 .article-6").mouseenter(function() {
 
     $("#portofolio .section-2 .article-6 .p").css("opacity", "1");
